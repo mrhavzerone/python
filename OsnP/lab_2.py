db654153b42f8a47694c1cc0a2765e27087c538b
mrhavzerone <mrhavzerone@proton.me>
2024-06-13 17:34:49 +0300

    lab_2 fuctions rebuild

--- OsnP/lab_2.py
+++ OsnP/lab_2.py
@@ -1,1 +1,16 @@
-lab_2.py
+class Programm():
+    user_data = {
+        'name': None,
+        'age': None,
+        'bdate': None
+    }
+
+    def hello():
+        for keys in Programm.user_data.keys():
+            Programm.user_data[keys] = input("Enter your " + keys + ' ')
+
+    def parrot(word, modiifier):
+        return word*modiifier
+
+    def swap(a, b):
+        return b, a
